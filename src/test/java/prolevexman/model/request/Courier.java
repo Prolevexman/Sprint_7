acc023a04fc5e57f477a1057bc631d0e8cbf97d6
Prolevexman <Prolevexman@gmail.com>
2025-08-28 01:31:24 +0300

    Редактировать модели курьера

--- src/test/java/prolevexman/model/request/Courier.java
+++ src/test/java/prolevexman/model/request/Courier.java
@@ -37,4 +37,9 @@ public class Courier {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return String.format("Курьер: firstName=%s", firstName);
+    }
 }
--- src/test/java/prolevexman/model/response/CourierResponse.java
+++ src/test/java/prolevexman/model/response/CourierResponse.java
@@ -2,6 +2,15 @@ package prolevexman.model.response;
 
 public class CourierResponse {
     private boolean ok;
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 
     public boolean isOk() {
         return ok;
